228b66cdaa4f945cd0cce8b260e07c4ffe7d485f
Antonios Papadakis <bdpaboi123@gmail.com>
2021-06-26 17:43:15 -0500

    Generate XWP Address
    I need 2 versions of myswap-core-js for it to work atm, so maybe I'll clean it up later

--- src/init.js
+++ src/init.js
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import cors from 'cors';
-import express, { response } from 'express';
+import express from 'express';
 
 const app = express();
 
@@ -15,35 +15,15 @@ app.get('/', (req, res) => {
 });
 
 app.get('/create_wallet', (req, res) => {
-
-  async function create_wallet()
-  {
-    const monero_utils = await mymonero.monero_utils_promise;
-
-    const nettype = mymonero.nettype_utils.network_type.MAINNET; // only use mainnet for now
-    const new_wallet = monero_utils.newly_created_wallet("english", nettype);
-
+    const walletUtils = require('./myswap-core-js-1.0.0/monero_utils/monero_wallet_utils');
+    const new_wallet = walletUtils.NewlyCreatedWallet('english', 0);
     const wallet = {
-      mnemonic: new_wallet.mnemonic_string,
-      wallet_address: new_wallet.address_string,
-      public_spendKey: new_wallet.pub_spendKey_string,
-      public_viewKey: new_wallet.pub_viewKey_string,
-      private_spendKey: new_wallet.sec_spendKey_string,
-      private_viewKey: new_wallet.sec_viewKey_string,
+      mnemonic: new_wallet.mnemonicString,
+      wallet_address: new_wallet.keys.public_addr,
+      spendKey: new_wallet.keys.spend.sec,
+      viewKey: new_wallet.keys.view.sec,
     };
-
-    return JSON.stringify(wallet);
-  }
-  create_wallet().then((wallet) => {
-    res.send(wallet);
-  }).catch((error) => {
-    res_send = {
-      status: "error",
-      error: error,
-    }
-    res.send(JSON.stringify(res_send));
-  });
-
+    res.send(JSON.stringify(wallet));
 });
 
 app.post('/send_funds', (req, res) => {
